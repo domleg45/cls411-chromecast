1032ca724979d1e75b0af287771cf2b0d69439fc
Dominic Légaré <dominic.legare@collegemv.qc.ca>
2024-03-07 12:49:02 -0500

    Test message custom

---
+++ gameReceiver.js
@@ -0,0 +1,14 @@
+const messageListener = (event) => {
+    const message = event.data;
+    if (message.type === 'customMessageType') {
+      // Gérez un type de message personnalisé
+      const customData = message.customData;
+      console.log('Message personnalisé reçu :', customData);
+      var s = document.getElementById("test");
+      s.value = customData;
+    }
+  };
+  
+  // Ajoutez le gestionnaire d'événements au récepteur
+  const receiverManager = cast.framework.CastReceiverContext.getInstance().getCastReceiverManager();
+  receiverManager.addEventListener(cast.framework.system.EventType.MESSAGE, messageListener);
