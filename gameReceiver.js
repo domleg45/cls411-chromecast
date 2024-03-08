8af3eda0a757b3efa08aee01fbdf5915061a594f
Dominic Légaré <dominic.legare@collegemv.qc.ca>
2024-03-08 09:34:43 -0500

    Changement context listener namespace urn:x-cast:mvlab 2

--- gameReceiver.js
+++ gameReceiver.js
@@ -1,14 +1,17 @@
  
 // Ajoutez le gestionnaire d'événements au récepteur
 const context = cast.framework.CastReceiverContext.getInstance();
-context.addEventListener(cast.framework.system.EventType.MESSAGE, messageListener);
+context.addCustomMessageListener('urn:x-cast:mvlab', cast.framework.system.EventType.MESSAGE, messageListener);
 
-messageBus = receiverManager.getCastMessageBus('urn:x-cast:mvlab', cast.receiver.CastMessageBus.MessageType.JSON);
-
-messageBus.onMessage = function(event) {
-  var sender = event.senderId;
-  var message = event.data;
-  var s = document.getElementById("test");
-  s.innerHTML = message;
-  alert(message);
+messageBus = receiverManager.getCastMessageBus('urn:x-cast:mvlab', function(customEvent) {
+	if(customEvent.data.type == "message"){
+		  var sender = customEvent.senderId;
+		  var message = customEvent.data.value;
+		  var s = document.getElementById("test");
+		  s.innerHTML = message;
+		  alert(message);		
+	}
 };
+
+
+context.start();
