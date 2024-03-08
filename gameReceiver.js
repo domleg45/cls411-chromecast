214e75fb91fade5a3cd018e0d9facefb9132774d
Dominic Légaré <dominic.legare@collegemv.qc.ca>
2024-03-08 09:19:47 -0500

    Changement context listener namespace urn:x-cast:

--- gameReceiver.js
+++ gameReceiver.js
@@ -3,7 +3,7 @@
 const context = cast.framework.CastReceiverContext.getInstance();
 context.addEventListener(cast.framework.system.EventType.MESSAGE, messageListener);
 
-messageBus = receiverManager.getCastMessageBus('edu.mv.action', cast.receiver.CastMessageBus.MessageType.JSON);
+messageBus = receiverManager.getCastMessageBus('urn:x-cast:edu.mv.action', cast.receiver.CastMessageBus.MessageType.JSON);
 
 messageBus.onMessage = function(event) {
   var sender = event.senderId;
