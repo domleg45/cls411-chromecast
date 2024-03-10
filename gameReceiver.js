93de2e6ff6f3a2af1282e06c4c01f9093c01e3f3
Dominic Légaré <dominic.legare@collegemv.qc.ca>
2024-03-10 18:39:59 -0400

    Retrait handlemessage

--- gameReceiver.js
+++ gameReceiver.js
@@ -8,24 +8,12 @@ const namespace = 'urn:x-cast:com.transfertco.cast';
 
 playerManager.addEventListener(
   cast.framework.events.EventType.ALL, (event) => {
-	var message = event.data;
+	var message = "test 123";
     console.log(`Received message on namespace ${namespace}:`, message);	
 	var s = document.getElementById("test");
 	s.innerHTML = message;
    
 });
-playerManager.setMessageInterceptor(namespace, handleCustomMessage);
-
-function handleCustomMessage(namespace, message) {
-  // Handle your custom messages here
-  console.log(`Received message on namespace ${namespace}:`, message);
-
-  // You can send a response back if needed
-  // playerManager.sendCustomMessage(namespace, { response: 'Received your message!' });
-
-  return true;
-}
-
 
 // Start the receiver
 context.start();
