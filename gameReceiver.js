2040813e8f2b3fe1268c408180d54a155eda9214
Dominic Légaré <dominic.legare@collegemv.qc.ca>
2024-03-10 18:17:53 -0400

    Ajout playerManager 3

--- gameReceiver.js
+++ gameReceiver.js
@@ -1,22 +1,19 @@
 // Ajoutez le gestionnaire d'événements au récepteur
-  cast.framework.CastReceiverContext.getInstance().setLoggerLevel(cast.framework.LoggerLevel.DEBUG);
+cast.framework.CastReceiverContext.getInstance().setLoggerLevel(cast.framework.LoggerLevel.DEBUG);
 
-  const context = cast.framework.CastReceiverContext.getInstance();
-  const playerManager = context.getPlayerManager();
+const context = cast.framework.CastReceiverContext.getInstance();
+const playerManager = context.getPlayerManager();
 
-  const namespace = 'urn:x-cast:com.transfertco.cast';
+const namespace = 'urn:x-cast:com.transfertco.cast';
 
-  playerManager.setMessageInterceptor(namespace, handleMessage);
-
-  function handleMessage(namespace, message) {
-    console.log(`Received message on namespace ${namespace}:`, message);
-	
+playerManager.addEventListener(
+  cast.framework.events.EventType.ALL, (event) => {
+	var message = event.data;
+    console.log(`Received message on namespace ${namespace}:`, message);	
 	var s = document.getElementById("test");
 	s.innerHTML = message;
-    // You can send a response back if needed
-    // playerManager.sendCustomMessage(namespace, { response: 'Received your message!' });
-    return true;
-  }
+   
+});
 
-  // Start the receiver
-  context.start();
+// Start the receiver
+context.start();
