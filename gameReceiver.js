091ecb25d7b8af1c45d9add2b631ac651068a95e
Dominic Légaré <dominic.legare@collegemv.qc.ca>
2024-03-11 19:07:15 -0400

    Ajout channel custom 25

--- gameReceiver.js
+++ gameReceiver.js
@@ -1,3 +1,4 @@
+const castDebugLogger = cast.debug.CastDebugLogger.getInstance();
 const context = cast.framework.CastReceiverContext.getInstance();
 
 const CHANNEL = 'urn:x-cast:testChannel';
@@ -7,6 +8,13 @@ const CHANNEL = 'urn:x-cast:testChannel';
 //    parseCommand(eventData);
 //    idleTime = 0;
 //});
+
+context.addEventListener(cast.framework.system.EventType.READY, () => {
+  if (!castDebugLogger.debugOverlayElement_) {
+      // Enable debug logger and show a 'DEBUG MODE' overlay at top left corner.
+      castDebugLogger.setEnabled(true);
+  }
+});
 
 const options = new cast.framework.CastReceiverOptions();
 
