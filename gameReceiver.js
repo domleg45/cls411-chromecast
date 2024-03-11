27f105b91db51eecbc45b9a5e4158b978f1a34c0
Dominic Légaré <dominic.legare@collegemv.qc.ca>
2024-03-11 10:08:12 -0400

    Ajout channel custom 5

--- gameReceiver.js
+++ gameReceiver.js
@@ -1,21 +1,19 @@
-  const namespace = 'urn:x-cast:com.transfertco.cast1';
+  const namespace1 = 'urn:x-cast:com.transfertco.cast1';
+  const namespace2 = 'urn:x-cast:com.transfertco.cast2';
   const ctx = cast.framework.CastReceiverContext.getInstance();
-  const options = new cast.framework.CastReceiverOptions();
-  const objToSender = 
-  {
-    type: 'status',
-    message: 'Playing'
-  };
+  
 
-  options.customNamespaces = Object.assign({});
-  options.customNamespaces[namespace] = cast.framework.system.MessageType.JSON;
-
+  
   //receiving sender message
-  ctx.addCustomMessageListener(namespace,  
+  ctx.addCustomMessageListener(namespace1,  
 	customEvent => 
 		document.getElementById("test").innerHTML = customEvent.data.msg);
 
-  //message to sender app
-  ctx.sendCustomMessage(namespace, objToSender);
+	const options = new cast.framework.CastReceiverOptions();
+	options.customNamespaces = {
+		namespace1: cast.framework.system.MessageType.JSON
+	};
 
-  ctx.start(options);
+	options.disableIdleTimeout = true;
+
+	ctx.start(options);
