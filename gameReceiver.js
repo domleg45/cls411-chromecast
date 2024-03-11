b422a48e0f13643094690e3dba0fec29c7834a99
Dominic Légaré <dominic.legare@collegemv.qc.ca>
2024-03-11 08:37:30 -0400

    Ajout channel custom 2

--- gameReceiver.js
+++ gameReceiver.js
@@ -6,25 +6,12 @@ const playerManager = context.getPlayerManager();
 
 const namespace = 'urn:x-cast:com.transfertco.cast1';
 
-playerManager.addEventListener(
-  cast.framework.events.EventType.ALL, (event) => {
-	var message = "test 123";
-    console.log(`Received message on namespace ${namespace}:`, message);	
-	var s = document.getElementById("test");
-	s.innerHTML = message;
-   
-});
-
-
-playerManager.setMessageInterceptor(cast.framework.messages.MessageType.CUSTOM, handleChannelOneMessage);
-
-function handleChannelOneMessage(namespace, message) {
-    console.log(`Received message on channel one:`, message);
-    // Handle channel one messages
-    return true;
-}
-
-
+playerManager.addEventListener(cast.framework.events.category.CORE,
+    event => {
+        console.log(event);
+		var s = document.getElementById("test");
+		s.innerHTML = event.data;
+    });
 
 // Start the receiver
 context.start();
