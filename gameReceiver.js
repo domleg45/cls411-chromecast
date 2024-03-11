456f5599a8b00d32554b6930755ca03d25b3bd4c
Dominic Légaré <dominic.legare@collegemv.qc.ca>
2024-03-11 10:32:55 -0400

    Ajout channel custom 20

--- gameReceiver.js
+++ gameReceiver.js
@@ -1,22 +1,12 @@
-  const namespace1 = 'urn:x-cast:com.transfertco.cast1';
-  const namespace2 = 'urn:x-cast:com.transfertco.cast2';
+  const CHANNEL = 'urn:x-cast:com.transfertco.cast1';
   const ctx = cast.framework.CastReceiverContext.getInstance();
-  
-
-  
-
-
-	
-	
-	
-	
 
 	const options = new cast.framework.CastReceiverOptions();
-	options.customNamespaces = { 'urn:x-cast:com.transfertco.cast1': 'STRING' }
+	options.customNamespaces = { CHANNEL: 'STRING' }
 
 	options.disableIdleTimeout = true;
 
-    ctx.addCustomMessageListener('urn:x-cast:com.transfertco.cast1', function(customEvent) {
+    ctx.addCustomMessageListener(CHANNEL, function(customEvent) {
 	  //document.getElementById("test").innerHTML = customEvent;
     });
 
